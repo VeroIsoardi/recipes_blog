91de3cf6accbfdc260883ef22ff53cd4712d1b7e
Veronica Isoardi <veronica.isoardi@gmail.com>
2024-10-27 20:55:42 -0300

    Add auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   resources :recipes
+
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  get 'service-worker' => 'pwa#service_worker', as: :pwa_service_worker
+  get 'manifest' => 'pwa#manifest', as: :pwa_manifest
+
   root "recipes#index"
 end
